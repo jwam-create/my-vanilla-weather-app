c0f66a99ecf4de815a45f6db4361b7f62c4d9cad
jwam-create <77247458+jwam-create@users.noreply.github.com>
2021-12-13 20:08:31 -0500

    Connected weather API and completed JS for temperature and weather conditions

---
+++ src/script.js
@@ -0,0 +1,19 @@
+function displayTemperature(response) {
+	console.log(response.data);
+
+	let temperatureElement = document.querySelector("#temperature");
+	let humidityElement = document.querySelector("#humidity");
+	let windElement = document.querySelector("#wind");
+	let descriptionElement = document.querySelector("#description");
+
+	temperatureElement.innerHTML = Math.round(response.data.main.temp);
+	humidityElement.innerHTML = response.data.main.humidity;
+	windElement.innerHTML = Math.round(response.data.wind.speed);
+	descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let city = "London";
+let apiKey = "3ef1c4739274de1e0c3fc584c54fc2ec";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
