22577d86616eda08e4a23dee32145ce821b21f55
jwam-create <77247458+jwam-create@users.noreply.github.com>
2021-12-13 21:54:10 -0500

    Added city display to display temperature function in JS

--- src/script.js
+++ src/script.js
@@ -1,11 +1,13 @@
 function displayTemperature(response) {
 	console.log(response.data);
 
+	let cityElement = document.querySelector("#city");
 	let temperatureElement = document.querySelector("#temperature");
 	let humidityElement = document.querySelector("#humidity");
 	let windElement = document.querySelector("#wind");
 	let descriptionElement = document.querySelector("#description");
 
+	cityElement.innerHTML = response.data.name;
 	temperatureElement.innerHTML = Math.round(response.data.main.temp);
 	humidityElement.innerHTML = response.data.main.humidity;
 	windElement.innerHTML = Math.round(response.data.wind.speed);
@@ -13,6 +15,7 @@ function displayTemperature(response) {
 }
 
 let city = "London";
+
 let apiKey = "3ef1c4739274de1e0c3fc584c54fc2ec";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
