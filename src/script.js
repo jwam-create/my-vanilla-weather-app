30ad6ad9a6e2a89ce23118bd27246a5626dc04aa
jwam-create <77247458+jwam-create@users.noreply.github.com>
2021-12-18 15:39:53 -0500

    Added live date

--- src/script.js
+++ src/script.js
@@ -1,3 +1,30 @@
+function displayDate(timestamp) {
+	let date = new Date(timestamp);
+	let hours = date.getHours();
+
+	if (hours < 10) {
+		hours = `0${hours}`;
+	}
+
+	let minutes = date.getMinutes();
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	let day = days[date.getDay()];
+
+	return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
 	console.log(response.data);
 
@@ -6,12 +33,14 @@ function displayTemperature(response) {
 	let humidityElement = document.querySelector("#humidity");
 	let windElement = document.querySelector("#wind");
 	let descriptionElement = document.querySelector("#description");
+	let dateElement = document.querySelector("#date");
 
 	cityElement.innerHTML = response.data.name;
 	temperatureElement.innerHTML = Math.round(response.data.main.temp);
 	humidityElement.innerHTML = response.data.main.humidity;
 	windElement.innerHTML = Math.round(response.data.wind.speed);
 	descriptionElement.innerHTML = response.data.weather[0].description;
+	dateElement.innerHTML = displayDate(response.data.dt * 1000);
 }
 
 let city = "Kitchener";
